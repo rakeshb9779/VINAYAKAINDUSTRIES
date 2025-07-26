d5bfb8af3c31b5d13a4df69c329cc5aea5e277ef
Rakesh Babu B <83860968+rakeshb9779@users.noreply.github.com>
2025-07-26 18:05:48 +0530

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,12 +1,18 @@
 export default async (request, context) => {
   const url = "https://script.google.com/macros/s/AKfycbytvxDO1jlUxa289NXACCuRj0osY4Idep6WXaNC_rJ9WR_6h1pUYfWpBdtsI-AfuGkB/exec";
-  
-  const resp = await fetch(url, {
+
+  // Prepare options, add body and duplex ONLY for POST/PUT/PATCH etc
+  let options = {
     method: request.method,
-    headers: { 'content-type': 'application/json' },
-    body: request.body,
-    duplex: "half",
-  });
+    headers: { 'content-type': 'application/json' }
+  };
+
+  if (request.method !== "GET" && request.body) {
+    options.body = request.body;
+    options.duplex = "half";
+  }
+
+  const resp = await fetch(url, options);
   const data = await resp.text();
 
   return new Response(data, {
