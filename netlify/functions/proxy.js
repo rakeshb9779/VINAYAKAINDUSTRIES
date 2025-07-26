cd1c20d4eecbf6458351bc11201b90cfb0591e61
Rakesh Babu B <83860968+rakeshb9779@users.noreply.github.com>
2025-07-26 15:54:56 +0530

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,15 +1,14 @@
 export default async (request, context) => {
   const url = "https://script.google.com/macros/s/AKfycbytvxDO1jlUxa289NXACCuRj0osY4Idep6WXaNC_rJ9WR_6h1pUYfWpBdtsI-AfuGkB/exec";
   
-  // Forward the request (method, headers, body)
   const resp = await fetch(url, {
     method: request.method,
     headers: { 'content-type': 'application/json' },
     body: request.body,
+    duplex: "half",
   });
   const data = await resp.text();
 
-  // Use the Response class
   return new Response(data, {
     status: resp.status,
     headers: {
